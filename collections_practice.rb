b1be9aec2985c88b5a960f9d9729fc814620776c
Patrick McCombs <pa.mccombs1@gmail.com>
2018-07-06 12:30:05 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,26 +1,26 @@
 def sort_array_asc(array)
 end
 
-def sort_array_desc()
+def sort_array_desc(array)
 end
 
-def sort_array_char_count()
+def sort_array_char_count(array)
 end
 
-def swap_elements()
+def swap_elements(array)
 end
 
-def reverse_array()
+def reverse_array(array)
 end
 
-def kesha_maker()
+def kesha_maker(array)
 end
 
-def find_a()
+def find_a(array)
 end
 
-def sum_array()
+def sum_array(array)
 end
 
-def add_s
+def add_s(array)
 end
