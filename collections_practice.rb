421471a78a1e386e01f0e07de4c996ed3089abb2
Patrick McCombs <pa.mccombs1@gmail.com>
2018-07-06 14:02:56 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,54 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+  a <=> b
+end
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+  b <=> a
+end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+  a.length <=> b.length
+end
+end
+
+def swap_elements(array)
+  array.sort do |a, b|
+  a[1] <=> b[2]
+end
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |a|
+    a[2] = "$"
+  end
+  array
+end
+
+def find_a(array)
+  array.find_all do |a|
+    a.start_with?("a")
+  end
+end
+
+def sum_array(array)
+ array.inject(0) do |sum,x| 
+   sum + x 
+ end
+end
+
+def add_s(array)
+  array.each do |a|
+    next if a == array[1]
+    a << "s"
+    
+  end
+end
